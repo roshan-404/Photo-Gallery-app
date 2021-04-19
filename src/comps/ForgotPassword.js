55481611ff5666f3b9b207086a8cee862dc4cab7
Roshan Kumar Ojha <roshanboyrko@gmail.com>
2021-04-20 02:12:14 +0530

    Bug Fixed

--- src/comps/ForgotPassword.js
+++ src/comps/ForgotPassword.js
@@ -2,7 +2,7 @@
 import React, { useRef, useState} from 'react';
 import { Card, Form, Button, Alert } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthContext';
-import { Link, useHistory } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 export default function ForgotPassword() {
 
@@ -11,7 +11,7 @@ export default function ForgotPassword() {
     const { resetPassword } = useAuth()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
-    const history =useHistory()
+    // const history =useHistory()
     const [message, setMessage] = useState("")
 
     async function handleSubmit(e) {
@@ -22,7 +22,7 @@ export default function ForgotPassword() {
             setError('')
             setLoading(true)
             await resetPassword(emailRef.current.value)
-            history.push("/")
+            // history.push("/")
             setMessage("Check your inbox for further instructions ")
         }catch {
             setError('Failed to reset password')
@@ -43,9 +43,9 @@ export default function ForgotPassword() {
                             <Form.Label>Email</Form.Label>
                             <Form.Control type="email" ref={emailRef} required />
                         </Form.Group>
-                        <Button disabled={loading} className="w-100" type="submmit">Log In</Button>
+                        <Button disabled={loading} className="w-100" type="submmit">Reset Password</Button>
                         <div className="w-100 text-center mt-3">
-                            <Link to="/login">Log In</Link>
+                             <Link to="/login">Log In</Link>
                         </div>
                     </Form>
                 </Card.Body>
